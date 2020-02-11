77122d82f2601d7e1d82a09fce906d37a1bb43b6
Shova-Acharya <shova.723@gmail.com>
2020-02-11 22:17:33 +0545

    Add initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+ function todoList(){ 
+    //1. Creating a new Div
+    var newDiv = document.createElement('div');
+    newDiv.id = "newTask";
+    
+    //2. Creating checklist
+    var newItem = document.createElement('input');
+    newItem.id = 'newToDo';
+    newItem.type = 'checkbox';
+    
+    //3. creating text tag
+    var span = document.createElement('label');
+    span.className = "newText";
+    
+    //4. Creating delete button
+    var del = document.createElement('button');
+    del.innerHTML = "Delete"
+    del.onclick = function(){
+        var remove = document.activeElement.parentElement;
+        remove.parentNode.removeChild(remove);
+    };
+    
+    //5. Taking the text
+    var item = document.getElementById("toDoText");
+    var itemText = document.createTextNode(item.value);
+    span.appendChild(itemText);
+     
+    //6. Creating a family
+    newDiv.appendChild(newItem);
+    newDiv.appendChild(span);
+    newDiv.appendChild(del);
+    
+    //7. Adding new div 
+    document.getElementById('add').appendChild(newDiv);
+}
+
+
+
+
+
